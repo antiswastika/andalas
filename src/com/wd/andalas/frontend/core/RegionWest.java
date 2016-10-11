453fbc2fa12b0724dd774b57140397f739fe9037
Anonymous <ariv.wd@gmail.com>
2016-10-11 08:42:49 +0700

    Cleansing Code Dari Kantor

--- src/com/wd/andalas/frontend/core/RegionWest.java
+++ src/com/wd/andalas/frontend/core/RegionWest.java
@@ -2,8 +2,6 @@ package com.wd.andalas.frontend.core;
 
 import java.util.HashMap;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
@@ -35,58 +33,11 @@ public class RegionWest implements IsWidget {
 		westData.setCollapseMini(false);
 		westData.setMargins(new Margins(0, 4, 0, 5));
 		westData.setCollapseHeaderVisible(true);
-
-		// Membuat ImageList Menu A
-		VBoxLayoutContainer imp_A = new VBoxLayoutContainer();
-		imp_A.setVBoxLayoutAlign(VBoxLayoutAlign.CENTER);
-		imp_A.setPadding(new Padding(10));
-		Image menu_A1 = new Image();
-		menu_A1.setUrl("images/icon/48x48/daftar_pegawai.png");
-		menu_A1.setHeight("50px");
-		menu_A1.addClickHandler(menu_A1_clickHandler());
-
-		imp_A.add(menu_A1);
-		Label lbl_A1 = new Label("Pegawai THOS");
-		imp_A.add(lbl_A1);
-		/*Button btn = new Button("<a class='label-imgList'><img src='images/icon/48x48/daftar_pegawai.png'/><BR />Pegawai THOS</a>");
-		imp_A.add(btn);*/
-		ContentPanel cp_A = new ContentPanel();
-		cp_A.setHeading("Daftar");
-		cp_A.add(imp_A);
-
-		// Membuat ImageList Menu B
-		TextButton buttonAlert2 = new TextButton("Show Message");
-		ContentPanel cp_B = new ContentPanel();
-		cp_B.setHeading("Pengaturan");
-		cp_B.add(buttonAlert2);
-
-		TextButton buttonAlert3 = new TextButton("Show Message");
-		buttonAlert3.addSelectHandler(btnAlert_clickHandler());
-		ContentPanel cp_C = new ContentPanel();
-		cp_C.setHeading("Test");
-		cp_C.add(buttonAlert3);
-
-		/*Buat Accordion*/
-		AccordionLayoutContainer accdn = new AccordionLayoutContainer();
-		accdn.setExpandMode(ExpandMode.SINGLE_FILL);
-		accdn.addStyleName("label-imgList");
-		accdn.add(cp_A);
-		accdn.setActiveWidget(cp_A);
-		accdn.add(cp_B);
-		accdn.add(cp_C);
-		west.add(accdn);
-
+		
+		MenuLeft menuLeft = (MenuLeft) outerObjects.get("menuLeft");
+		west.add(menuLeft);
+		
 		return west;
-	}
-
-	private ClickHandler menu_A1_clickHandler() {
-		return new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				RegionTabPanel tabPanel = (RegionTabPanel) outerObjects.get("regionTabPanel");
-				tabPanel.doCreateTab(tabPanel.getTabPanel(), 1);
-			}
-		};
 	}
 
 	/*********************************** SETTER GETTER ***********************************/
