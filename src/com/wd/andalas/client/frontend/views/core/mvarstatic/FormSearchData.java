e28a96c8c64f6aa6ac59a087d69890e1cda3f798
arivwd <ariv.wd@gmail.com>
2017-04-04 00:38:04 +0700

    FormSearch Hampir JAdi

--- src/com/wd/andalas/client/frontend/views/core/mvarstatic/FormSearchData.java
+++ src/com/wd/andalas/client/frontend/views/core/mvarstatic/FormSearchData.java
@@ -27,6 +27,7 @@ import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
 import com.sencha.gxt.widget.core.client.form.ComboBox;
 import com.sencha.gxt.widget.core.client.form.FieldLabel;
+import com.sencha.gxt.widget.core.client.form.TextField;
 import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
 import com.wd.andalas.global.GlobalToolbarList;
 import com.wd.andalas.global.views.AnyComboModel;
@@ -57,8 +58,8 @@ public class FormSearchData extends VBoxLayoutContainer implements IsWidget {
 	/********** Custom Methods **********/
 	private HorizontalLayoutContainer doCreateForm() {
 		hlcMain = new HorizontalLayoutContainer();
-		Boolean debugShowBorders = true;
-		int banyakComboBox = 5;
+		Boolean debugShowBorders = false;
+		int banyakComboBox = 6;
 
 		//Buat Kolom 1
 		//===========================================================
@@ -74,24 +75,20 @@ public class FormSearchData extends VBoxLayoutContainer implements IsWidget {
 		for (int i=0; i<banyakComboBox; i++) {
 			ComboBox<AnyComboModel> cmbField;
 			cmbField = doCreateComboboxField(comboValues);
-			cmbField.setEmptyText("Pilih field...");
+			cmbField.setEmptyText("- - -");
 			cmbField.setEditable(false);
 			cmbField.setTriggerAction(TriggerAction.ALL);			
-			vlcCol1.add(new FieldLabel(cmbField, "Field " + (i+1)), new VerticalLayoutData(-1, -1));
+			vlcCol1.add(new FieldLabel(cmbField, "Field " + (i+1)), new VerticalLayoutData(1, -1));
 			
 			ComboBox<AnyComboModel> cmbField2;
-			cmbField2 = doCreateComboboxField(comboValues);
-			cmbField2.setEmptyText("Pilih field...");
+			cmbField2 = doCreateComboboxCondition();
+			cmbField2.setEmptyText("- - -");
 			cmbField2.setEditable(false);
 			cmbField2.setTriggerAction(TriggerAction.ALL);	
-			vlcCol2.add(new FieldLabel(cmbField2, ""), new VerticalLayoutData(-1, -1));
+			vlcCol2.add(new FieldLabel(cmbField2, ""), new VerticalLayoutData(1, -1));
 			
-			ComboBox<AnyComboModel> cmbField3;
-			cmbField3 = doCreateComboboxField(comboValues);
-			cmbField3.setEmptyText("Pilih field...");
-			cmbField3.setEditable(false);
-			cmbField3.setTriggerAction(TriggerAction.ALL);	
-			vlcCol3.add(new FieldLabel(cmbField3, ""), new VerticalLayoutData(-1, -1));
+			TextField textField = new TextField();
+			vlcCol3.add(new FieldLabel(textField, ""), new VerticalLayoutData(1, -1));
 		}
 		
 		Iterator<Widget> arrayOfChilds1 = vlcCol1.iterator();
@@ -126,9 +123,9 @@ public class FormSearchData extends VBoxLayoutContainer implements IsWidget {
 
 		//Terapkan
 		//===========================================================
-		hlcMain.add(vlcCol1, new HorizontalLayoutData(0.3, -1, new Margins(10, 1, 10, 10)));
-		hlcMain.add(vlcCol2, new HorizontalLayoutData(0.2, -1, new Margins(10, 1, 10, 1)));
-		hlcMain.add(vlcCol3, new HorizontalLayoutData(0.5, -1, new Margins(10, 10, 10, 1)));
+		hlcMain.add(vlcCol1, new HorizontalLayoutData(-1, -1, new Margins(10, 1, 10, 10)));
+		hlcMain.add(vlcCol2, new HorizontalLayoutData(90, -1, new Margins(10, 1, 10, 1)));
+		hlcMain.add(vlcCol3, new HorizontalLayoutData(1, -1, new Margins(10, 10, 10, 1)));
 
 		return hlcMain;
 	}
@@ -173,13 +170,44 @@ public class FormSearchData extends VBoxLayoutContainer implements IsWidget {
 			}
 		};
 
-		/* Step 5 : Buat combobox */
+		/* Step 4 : Buat combobox */
 		ComboBox<AnyComboModel> cmb = new ComboBox<AnyComboModel>(store, labelProvider);
 		cmb.setWidth(formTitle);
 
 		return cmb;
 	}
+	
+	private ComboBox<AnyComboModel> doCreateComboboxCondition() {
+		/* Step 1 : Buat Store */
+		ListStore<AnyComboModel> store = new ListStore<AnyComboModel>(new ModelKeyProvider<AnyComboModel>() {
+			public String getKey(AnyComboModel item) {
+				return item.getKey().toString();
+			}
+		});
 
+		/* Step 2 : Buat Data */
+		store.add(new AnyComboModel("0", "="));
+		store.add(new AnyComboModel("1", "<>"));
+		store.add(new AnyComboModel("2", "LIKE"));
+		
+		/* Step 3 : Buat labelProvider */
+		LabelProvider<AnyComboModel> labelProvider = new LabelProvider<AnyComboModel>() {
+			@Override
+			public String getLabel(AnyComboModel item) {
+				if (item.getValue() == null) {
+					return "";
+				}
+				return item.getValue();
+			}
+		};
+
+		/* Step 4 : Buat combobox */
+		ComboBox<AnyComboModel> cmb = new ComboBox<AnyComboModel>(store, labelProvider);
+		cmb.setWidth(formTitle);
+
+		return cmb;
+	}
+	
 	/********** Event Handler dan Listener **********/
 	private SelectHandler doSearch() {
 		return new SelectHandler() {
